40da506d7739c552cc954a7de0e4afca1d3090eb
yko <ykorshak@gmail.com>
2011-11-11 16:05:50 +0200

    bootstrapped ui. thanks twitter

--- htdocs/static/js/main.js
+++ htdocs/static/js/main.js
@@ -1,54 +1,57 @@
-function process_regexp_submit() {
-	var form_data = {
-			text: [$('#search_text').val()],
-			match: $('#regexp_match').val(),
-			replace: $('#regexp_replace').val(),
-			flags: {
-				i: $('#flag_i:checked').length,
-				s: $('#flag_s:checked').length,
-				m: $('#flag_m:checked').length,
-				x: $('#flag_x:checked').length,
-				a: $('#flag_a:checked').length,
-				u: $('#flag_u:checked').length,
-				l: $('#flag_l:checked').length,
-				g: $('#flag_g:checked').length
-			}
-	};
-	
-    $.postJSON('/api/execute', form_data, function(data) {
-    	if (data.status != 1) {
-    		alert(data.errmsg);
-    		return;
-    	}
-    	
-		preprocess_results(form_data.text[0], data.results);
-		draw_results_tree(data.results);
-   });
-}
+(function() {
+    function process_regexp_submit(e) {
+        e.preventDefault();
+        var form_data = {
+            text: [$('#search_text').val()],
+            match: $('#regexp_match').val(),
+            replace: $('#regexp_replace').val(),
+            flags: {
+                i: $('#flag_i:checked').length,
+                s: $('#flag_s:checked').length,
+                m: $('#flag_m:checked').length,
+                x: $('#flag_x:checked').length,
+                a: $('#flag_a:checked').length,
+                u: $('#flag_u:checked').length,
+                l: $('#flag_l:checked').length,
+                g: $('#flag_g:checked').length
+            }
+        };
 
+        $.postJSON('/api/execute', form_data, function(data) {
+            if (data.status != 1) {
+                alert(data.errmsg);
+                return;
+            }
 
-function preprocess_results(text, results) {
-	for ( var i = 0; i < results.length; i++ ) {
-		if (! results[i].found) continue;
-		for ( var j = 0; j < results[i].matches.length; j++ ) {
-			var match = results[i].matches[j];
-			extract_text_to_match(text, match);
-		}
-	}
-}
+            preprocess_results(form_data.text[0], data.results);
+            draw_results_tree(data.results);
+        });
+    }
 
-function extract_text_to_match(text, match) {
-	match.text = text.substr(match.from, match.to - match.from);
-	for ( var i = 0; i < match.groups.length; i++ ) {
-		var group = match.groups[i];
-		group.text = text.substr(group.from, group.to - group.from);
-	}
-}
+    function preprocess_results(text, results) {
+        for (var i = 0; i < results.length; i++) {
+            if (!results[i].found) continue;
+            for (var j = 0; j < results[i].matches.length; j++) {
+                var match = results[i].matches[j];
+                extract_text_to_match(text, match);
+            }
+        }
+    }
 
-function draw_results_tree(results) {
-	$("#result").empty().append(JSON.stringify(results));
-}
+    function extract_text_to_match(text, match) {
+        match.text = text.substr(match.from, match.to - match.from);
+        for (var i = 0; i < match.groups.length; i++) {
+            var group = match.groups[i];
+            group.text = text.substr(group.from, group.to - group.from);
+        }
+    }
 
+    function draw_results_tree(results) {
+        $("#result").empty().append(JSON.stringify(results));
+    }
 
+    $(function() {
+        $("#match_regexp_form").submit(process_regexp_submit)
+    });
 
-	  
+})(jQuery);
