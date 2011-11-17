c3f89f3e6e528bfbd0e3d9a8a99b308001748b6b
yko <ykorshak@gmail.com>
2011-11-17 13:13:03 +0200

    documented JS API

--- htdocs/static/js/jquery-relaxer-api.js
+++ htdocs/static/js/jquery-relaxer-api.js
@@ -1,5 +1,56 @@
 (function($) {
 
+    /* Relaxer API JavaScript bindings
+     *
+     * Usage:
+     *
+     *     $("form.selector").relaxer();
+     *     $("form.selector").relaxer({type: "match"});
+     *     $("form.selector").relaxer({type: "replace"});  // not supported yet
+     *
+     * Binds given form to Relaxer API.
+     * Plugin expects the form to have following elements:
+     * 
+     *     [name=regexp_match]
+     *     textarea or input containing regexp
+     *     required
+     *
+     *     [name=search_text]
+     *     textarea or input containing text
+     *     required
+     *
+     *     checkbox[name=flag_(i|s|m|x|a|u|l|g)]
+     *     checkbocks that represents regexp flags
+     *     optional
+     *
+     *
+     *  Plugin emits following events:
+     *
+     *  relaxer_match_done
+     *  Relaxer API successfull completed match of given text against given regexp
+     *  This event guaraniee that regexp compiled successfully and there was no errors during match.
+     *  This even does not guaranee something was mached, only match finished with no errors.
+     *  To find what was matched you need to loop over `event.results` array and look into  `found` property of each element
+     *  Event object fields:
+     *  
+     *      event.string       // string that was used to match
+     *      event.results      // array of matches
+     *      event.groups       // information about groups in regexp
+     *      event.regexp       // original regexp
+     *
+     *  relaxer_match_error
+     *  Relaxer API returned error after trying to execute given regexp
+     *  Opposite to `relaxer_match_done`
+     *  Usually this means that regexp contains error.
+     *
+     *  Event object fields:
+     *
+     *      event.string // string that was used to match
+     *      event.results      // array of matches
+     *      event.groups       // information about groups in regexp
+     *      event.regexp       // original regexp
+     */
+
     var methods = {
         init: function(options) {
             if (options == undefined || options.type == undefined || options.type == "match") {
@@ -52,18 +103,23 @@
         };
 
         $.postJSON('/api/execute', form_data, function(data) {
+            // TODO: add types of errror: regep, network, unknown
+            // TODO: catch network errors of postJSON and rethrow them as relaxer_match_error
             if (data.status != 1) {
                 $(document).trigger({
                     type: 'relaxer_match_error',
-                    message: data.errmsg
+                    message: data.errmsg,
+                    regexp: form_data.match,
+                    string: form_data.text[0]
                 });
                 return;
             }
 
             $(document).trigger({
                 type: 'relaxer_match_done',
-                string: form_data.text[0],
                 results: data.results,
+                string: form_data.text[0],
+                regexp: form_data.match,
                 groups: data.groups
             });
         });
